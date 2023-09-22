42888300488644a84519440e257c4e65c291730e
ronkapoor123 <145025068+ronkapoor123@users.noreply.github.com>
2023-09-22 10:16:08 -0400

    ROhan

--- src/Food.java
+++ src/Food.java
@@ -1,2 +1,20 @@
+
 public class Food {
-}
+    // Fields
+    String name;
+    String subject;
+
+
+    void Dedication() {
+        System.out.println("int temperature+string style"+ name);
+    };
+
+    void Saturation() {
+        System.out.println("int temperature+string style"+ name);
+    };
+
+    void Nutrition () {
+        System.out.println("int temperature+string style" + name);
+    }};
+
+
